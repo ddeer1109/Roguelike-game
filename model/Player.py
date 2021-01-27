536e8eb6d903f577ddfb652ca0986d23b88af6aa
Dariusz Okonski <darek200180@gmail.com>
2021-01-27 12:49:30 +0000

    create file Player and change player.py to creature.py

--- model/Player.py
+++ model/Player.py
@@ -1,26 +1,4 @@
-from constants import PLAYER
-
-class Creature:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.icon = PLAYER
-
-
-    def __str__(self):
-        return self.icon
-
-    def move_up(self):
-        self.x -= 1
-
-    def move_down(self):
-        self.x += 1
-        
-    def move_left(self):
-        self.y -= 1
-
-    def move_right(self):
-        self.y += 1
+from Creature import Creature
 
 class Player(Creature):
     def __init__(self, x, y):
