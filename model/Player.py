34a2101d1f4b3f686ddc3066dd234936fff2929e
Dariusz Okonski <darek200180@gmail.com>
2021-01-28 13:31:53 +0000

    remove comment

--- model/Player.py
+++ model/Player.py
@@ -1,9 +1,9 @@
 from model.Creature import Creature
-from model.constants import PLAYER
+from model.constants import PLAYER, CENTRAL_ROOM
 
 class Player(Creature):
     def __init__(self, x, y):
         super().__init__(x, y)
         self.icon = PLAYER
         self.health = 100
-        self.current_field = None
+        self.current_room = CENTRAL_ROOM
