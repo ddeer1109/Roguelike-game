c6b45991bddc5ae1e99de01cdc22d3736db11848
Cezary <cyarek@gmail.com>
2021-01-30 12:45:48 +0100

    changing static methot to class method in game controller

--- controller/game_controller.py
+++ controller/game_controller.py
@@ -1,6 +1,7 @@
 from builtins import next
 import sys
 import os.path
+
 sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 
@@ -12,6 +13,7 @@ from model.board_objects.Wall import Wall
 from model.board_objects.Gate import Gate
 from model.constants import UPPER, BOTTOM, LEFT, RIGHT
 from model.items.Key import Key
+
 PLAYER_ICON = '@'
 PLAYER_START_X = 3
 PLAYER_START_Y = 3
@@ -19,119 +21,25 @@ PLAYER_START_Y = 3
 BOARD_WIDTH = 30
 BOARD_HEIGHT = 20
 
+
 class Main:
-    @staticmethod
-    def service_player_inputs(key_pressed, room, player):
+    @classmethod
+    def service_player_inputs(cls, key_pressed, room, player):
         k_pressed = str.lower(key_pressed)
-        
-        current_room = room 
-        
-        if k_pressed in ["w","a","s","d"]:
-            current_room = Main.service_pressing_move_key(k_pressed, room, player)
-                
+
+        current_room = room
+
+        if k_pressed in ["w", "a", "s", "d"]:
+            current_room = cls.service_pressing_move_key(k_pressed, room, player)
+
         return current_room
 
-
-
-
-    """BEFORE REFACTOR"""
-    # if k_pressed == "w":
-    #     next_area = type(room.fields[player.x-1][player.y])
-    #     next_object = room.fields[player.x-1][player.y]
-        
-    #     if next_area is Gate:
-    #         return current_room.gates[UPPER].go_through_gate(player, UPPER)
-    #     elif next_area is not Wall:
-    #         if next_area is Key:
-    #             player.inventory.append(next_object)
-
-    #         room.service_move_up(player)    
-    #         return current_room    
-    
-    # elif k_pressed == "s":
-    #     next_area = type(room.fields[player.x+1][player.y])
-    #     next_object = room.fields[player.x+1][player.y]
-        
-    #     if next_area is Gate:
-    #         return current_room.gates[BOTTOM].go_through_gate(player, BOTTOM)
-    #     elif next_area is not Wall:
-    #         if next_area is Key:
-    #             player.inventory.append(next_object)
-
-    #         room.service_move_down(player)
-    #         return current_room
-    
-    # elif k_pressed == "a":
-    #     next_area = type(room.fields[player.x][player.y-1])
-    #     next_object = room.fields[player.x][player.y-1]
-        
-    #     if next_area is Gate:
-    #         return current_room.gates[LEFT].go_through_gate(player, LEFT)
-    #     elif next_area is not Wall:
-    #         if next_area is Key:
-    #             player.inventory.append(next_object)
-
-    #         room.service_move_left(player)
-    #         return current_room
-
-    # elif k_pressed == "d":
-    #     # next_area = type(room.fields[player.x][player.y+1])
-    #     # next_object = room.fields[player.x][player.y+1]
-        
-    #     # if next_area is Gate:
-    #     #     current_gate = current_room.gates[RIGHT]
-            
-    #     #     if not current_gate.is_opened:
-    #     #         if current_gate.oppening_key in player.inventory:
-    #     #             current_gate.open_gate()
-    #     #             next_room = current_gate.go_through_gate(player, RIGHT)
-    #     #             next_room.gates[LEFT].open_gate()
-    #     #             return next_room
-    #     #     else:
-    #     #         return current_gate.go_through_gate(player, RIGHT)
-
-
-    #     # elif next_area is not Wall:
-    #     #     if next_area is Key:
-    #     #         player.inventory.append(next_object)
-
-    #     #     room.service_move_right(player)
-    #     #     return current_room
-
-
-    """FIRST ATTEMP OF REFACTOR"""
-    # def service_pressing_a(room, player):
-    #     next_object = room.fields[player.x][player.y-1]
-    #     current_room = room
-
-    #     if type(next_object) is Gate:
-    #         current_gate = room.gates[LEFT]
-
-    #         if not current_gate.is_opened:
-    #             if current_gate.oppening_key in player.inventory:
-    #                 del player.inventory[player.inventory.index(current_gate.oppening_key)]
-    #                 current_gate.open_gate()
-    #                 current_room.gates[RIGHT].open_gate()
-
-    #                 current_room = current_gate.go_through_gate(player, LEFT)
-    #         else:
-    #             current_room = current_gate.go_through_gate(player, LEFT)
-        
-    #     elif type(next_object) is not Wall:
-    #         if type(next_object) is Key:
-    #             player.inventory.append(next_object)
-            
-    #         room.service_move_right(player)
-        
-    #     return current_room
-
-    """FINAL (?) VERSION OF REFACTOR"""
     def get_data_after_key_press(pressed_key, player):
         if pressed_key == "w":
             modified_x, modified_y = player.x - 1, player.y
             direction = UPPER
 
-        elif pressed_key == "s": 
+        elif pressed_key == "s":
             modified_x, modified_y = player.x + 1, player.y
             direction = BOTTOM
 
@@ -142,12 +50,12 @@ class Main:
         elif pressed_key == "d":
             modified_x, modified_y = player.x, player.y + 1
             direction = RIGHT
-        
+
         return modified_x, modified_y, direction
 
     def service_pressing_move_key(key_pressed, room, player):
         modified_player_x, modified_player_y, direction = Main.get_data_after_key_press(key_pressed, player)
-        
+
         next_object = room.fields[modified_player_x][modified_player_y]
         current_room = room
 
@@ -156,25 +64,28 @@ class Main:
 
             if not current_gate.is_opened:
                 if current_gate.oppening_key in player.inventory:
-                    
                     del player.inventory[player.inventory.index(current_gate.oppening_key)]
                     current_gate.open_gate()
                     current_room = current_gate.go_through_gate(player, direction)
-                    
+
             else:
                 current_room = current_gate.go_through_gate(player, direction)
-        
+
         elif type(next_object) is not Wall:
             if type(next_object) is Key:
                 player.inventory.append(next_object)
-            
-            if direction == UPPER: room.service_move_up(player)
-            elif direction == BOTTOM: room.service_move_down(player)
-            elif direction == LEFT: room.service_move_left(player)
-            elif direction == RIGHT: room.service_move_right(player)
+
+            if direction == UPPER:
+                room.service_move_up(player)
+            elif direction == BOTTOM:
+                room.service_move_down(player)
+            elif direction == LEFT:
+                room.service_move_left(player)
+            elif direction == RIGHT:
+                room.service_move_right(player)
 
         return current_room
-        
+
     @staticmethod
     def main():
         print("START")
@@ -186,21 +97,19 @@ class Main:
         # here
         current_room = board.central_room
         is_running = True
-        
+
         ui.UI.display_room(current_room)
 
         while is_running:
             # util.clear_screen()
             ui.UI.display_room(current_room)
             ui.UI.display_statistics(player)
-            #Here changed mostly
+            # Here changed mostly
 
             key = util.Util.key_pressed()
-            
+
             if key == 'q':
                 is_running = False
             else:
-                #TODO - check cls reference instead of Main
+                # TODO - check cls reference instead of Main
                 current_room = Main.service_player_inputs(key, current_room, player)
-
-            
